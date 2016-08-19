386fd758d30746fd9721f9ae5857a6af61270b87
Olivia Hinton <hinton.olivia@gmail.com>
2016-08-19 13:14:10 -0700

    add update question functionality

--- app/routes/question.js
+++ app/routes/question.js
@@ -5,15 +5,6 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
-    update(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-        question.set(key,params[key]);
-        }
-      });
-        question.save();
-        this.transitionTo('question');
-    },
     saveAnswer(params) {
       var newAnswer = this.store.createRecord('answer', params);
       var question = params.question;
