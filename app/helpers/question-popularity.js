33249f7f2bec4133546f20deceeccda72d63d69a
Olivia Hinton <hinton.olivia@gmail.com>
2016-08-26 14:51:37 -0700

    add styles to favorites page and buttons

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -4,7 +4,7 @@ export function questionPopularity(params) {
   var question = params[0];
 
   if(question.get('answers').get('length') >= 5) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-heart"></span>');
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star-empty"></span>');
   }
 }
 
