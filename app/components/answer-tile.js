5abcb3306d5e61601f34b6e724dffa2e12529b86
Olivia Hinton <hinton.olivia@gmail.com>
2016-08-26 12:55:30 -0700

    working on favorites service

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteList: Ember.inject.service(),
 
+  actions: {
+    addToFaves(favorite) {
+      this.get('favoriteList').add(favorite);
+    }
+  }
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,13 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteList: Ember.inject.service(),
 
-  answerTotal: Ember.computed('question.answers.[]', function(){
+  answerTotal: Ember.computed('question.answers', function(){
     var total = 0;
-    this.get('question.answers').forEach(function(answer){
+    this.get('question.answers').forEach(function(){
       total += 1;
     });
     return total;
-  })
+  }),
+
+  actions: {
+    addToFaves(favorite) {
+      this.get('favoriteList').add(favorite);
+    }
+  }
 
 });
