6fe177e161e55b5ff5996dc1ab6e8ac2e2f50fa3
Olivia Hinton <hinton.olivia@gmail.com>
2016-08-26 10:12:17 -0700

    add computed property for answer total

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  answerTotal: Ember.computed('question.answers.[]', function(){
+    var total = 0;
+    this.get('question.answers').forEach(function(answer){
+      total += 1;
+    });
+    return total;
+  })
+
 });
